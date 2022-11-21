24f576cf4c2ed839b85837e4e785b59c36c88d73
FHP187 <gjimenez@jaumebalmes.net>
2022-11-21 20:10:04 +0100

    4rst push

---
+++ UF1/buscamines/part1/buscamines.js
@@ -0,0 +1,97 @@
+var columna;
+let matrix = [];
+
+var body = document.getElementsByTagName("body")[0];
+body.style.backgroundColor = "white"; //Color de fons de pantalla
+//Creem la funció inicialitzaJoc
+/*
+Exercici 1
+Crea una funció anomenada inicialitzaJoc associada amb un botó; de tal manera que quan premis el botó es creï una taula amb les dimensions especificades en els inputs.
+*/
+function inicialitzaJoc() {
+    /*// Generem les variables x - y*/
+    let x = document.getElementById("X").valueAsNumber;
+    let y = document.getElementById("Y").valueAsNumber;
+    /*Generem les variables tabla tblBody*/
+    var tabla = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+    /*Bucle for on generem la taula*/
+    for (var i = 0; i < y; i++) {
+        var fila = document.createElement("tr");
+        for (var j = 0; j < x; j++) {
+            /*Especifiquem els valors de la taula , color , mida de la cuadricula*/
+            columna = document.createElement("TD");
+            columna.textContent = ' ';
+            columna.style.border = "2x solid"; //mida del cuadrat
+            columna.style.color = "grey"; //color
+            columna.style.width = "50px"; //alçada
+            columna.style.height = "50px"; //allargada
+            columna.setAttribute("id",  j + " " + i);
+
+            fila.appendChild(columna);
+        }
+        tblBody.appendChild(fila);
+    }
+
+    tabla.appendChild(tblBody);
+
+    body.appendChild(tabla);
+    //Color de fons de la cuadricula
+    tblBody.style.backgroundColor = "grey";
+    //Posició de la cuadricula
+    tabla.style.marginLeft = "220px";
+
+    let rows = document.querySelector("tbody").children
+    //Bucle generador
+    for (var i = 0; i < rows.length; i++) {
+        matrix.push(rows[i].children)
+    }
+    tblBody.addEventListener("click", coordenadas);
+}
+
+function paintAll() {
+    for (let i =0; i < matrix.length ; i++) { // files
+        for (let j = 0; j < matrix[i].length ;j++ ) { // columnes
+            matrix[i][j].style.backgroundColor = "red";
+        }
+    }
+}
+
+
+function paintCheckerboard() {
+
+    for (let i = 0; i < matrix.length; i+=2) {
+        for (let j = 0; j < matrix[i].length; j+=2) {
+
+            matrix[i][j].style.backgroundColor = "red";
+        }
+    }
+    for (let y = 1; y < matrix.length; y+=2) {
+        for (let e = 1; e < matrix[y].length; e+=2) {
+            matrix[y][e].style.backgroundColor = "red";
+        }
+    }
+    let rows = document.querySelector("tbody").children
+
+    for (var i = 0; i < rows.length; i++) {
+        matrix.push(rows[i].children)
+    }
+
+}
+
+/*Exercici 2*/
+function matriuBinaria(matrix) {
+    var matrix2 = [];
+    for (var i = 0; i < matrix.length; i++) {
+        var fila = [];
+        for (var j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j].style.backgroundColor == "red") { //Pintem els 0 de vermell
+                fila.push(1);
+            } else {
+                fila.push(0);
+            }
+        }
+        matrix2.push(fila);
+    }
+    return matrix2;
+}
---
+++ UF1/buscamines/part2/buscamines.js
@@ -0,0 +1,106 @@
+var columna;
+let matrix = [];
+
+var body = document.getElementsByTagName("body")[0];
+body.style.backgroundColor = "white"; //Color de fons de pantalla
+//Creem la funció creataula
+function CreaTaula() {
+    /*// Generem les variables x - y*/
+    let x = document.getElementById("X").valueAsNumber;
+    let y = document.getElementById("Y").valueAsNumber;
+    /*Generem les variables tabla tblBody*/
+    var tabla = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+    /*Bucle for on generem la taula*/
+    for (var i = 0; i < y; i++) {
+        var fila = document.createElement("tr");
+        for (var j = 0; j < x; j++) {
+            /*Especifiquem els valors de la taula , color , mida de la cuadricula*/
+            columna = document.createElement("TD");
+            columna.textContent = ' ';
+            columna.style.border = "2x solid"; //mida del cuadrat
+            columna.style.color = "grey"; //color
+            columna.style.width = "50px"; //alçada
+            columna.style.height = "50px"; //allargada
+            columna.setAttribute("id",  j + " " + i);
+
+            fila.appendChild(columna);
+        }
+        tblBody.appendChild(fila);
+    }
+
+    tabla.appendChild(tblBody);
+
+    body.appendChild(tabla);
+    //Color de fons de la cuadricula
+    tblBody.style.backgroundColor = "grey";
+    //Posició de la cuadricula
+    tabla.style.marginLeft = "220px";
+
+    let rows = document.querySelector("tbody").children
+    //Bucle generador
+    for (var i = 0; i < rows.length; i++) {
+        matrix.push(rows[i].children)
+    }
+    tblBody.addEventListener("click", coordenadas);
+}
+
+let mina = [];
+
+/*
+# Exercici 1
+Feu una funció anomenada `inicialitzaMines`  que l'hi passis els paràmetres:
+- nMines: nombre de mines
+- midaX: Mida de la matriu horitzontal.
+- midaY: Mida de la matriu vertical
+i  **retorni** una matriu de `midaX` per `midaY` amb tantes mines com indicades pel paràmetre `nMines` posades de manera aleatòria.
+*/
+
+//Creem la funció inicialitzaMines
+function inicialitzaMines() {
+    let numMines = 0;
+    let nMines = document.getElementById("NumeroMines").valueAsNumber;
+    let X = document.getElementById("X").valueAsNumber;
+    let Y = document.getElementById("Y").valueAsNumber;
+    for (let a = 0; a < X; a++) {
+        mina[a] = [];
+        for (let b = 0; b < Y; b++) {
+            mina[a][b] = 0;
+        }
+    }
+    //Bucle while que genera aleatoriament la posició de les mines.
+    while (numMines < nMines){
+        let fila = Math.floor(Math.random() * Y);
+        let columna = Math.floor(Math.random() * X);
+        if (mina[fila][columna] == 0){
+            mina[fila][columna] = 1;
+            numMines++;
+        }
+    }
+    /*
+        # Exercici 3
+        Poseu un color diferent a les cel·les que continguin mines per tal de visualitzar que funcioni correctament.*/
+    for (let a = 0; a < X; a++) {
+        for (let b = 0; b < Y; b++) {
+            if (mina[a][b] == 1) {
+                matrix[a][b].style.backgroundColor = "red"; //Color de les mines
+            }
+        }
+    }
+
+}
+//Generem la funcio coordenades
+function coordenadas(event) {
+    let x = event.target.id.split(' ')[1];
+    let y = event.target.id.split(' ')[0];
+
+    if (event.target.tagName == "TD") {
+        console.log(event.target.id);
+    }
+    if (mina[x][y] == 1) {
+        return alert("MINA");
+    }
+    else {
+        return console.log("SALVAT");
+    }
+}
