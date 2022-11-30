04c04a148229d06a4ab50ed8f5d2173f3e8861d3
FHP187 <gjimenez@jaumebalmes.net>
2022-11-30 19:00:38 +0100

    Push assignatura

---
+++ UF2/Exercicis_Objectes/Assignatura.js
@@ -0,0 +1,13 @@
+// 0. Crea l'objecte Assignatura:
+
+var Asignatura = {
+    ref: 'BBDD',
+    nom: 'Bases de Dades',
+    hores: 180,
+    UF: ['UF1','UF2','UF3','UF4'],
+    professor: 'Pep Parés',
+    alumnes: ['alu1','alu2','alu13','alu4','alu5']
+};
+
+// 1. Amb JavaScript, llista les propietats de l'objecte Assignatura.
+
