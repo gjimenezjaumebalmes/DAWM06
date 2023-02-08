d1f5e736b03f22eb10ece172b3e89c8814d84598
FHP187 <gjimenez@jaumebalmes.net>
2023-02-08 20:52:45 +0100

    commit snake puntuacion

--- UF3/repas/index.js
+++ UF3/repas/index.js
@@ -3,11 +3,12 @@ function repas () {
     let canvas = document.getElementById("canvas");
     let context = canvas.getContext("2d");
 
-    const borde = "green";
+    const borde = "black";
     const fondo = "white";
 
     context.strokeStyle = borde;
     context.fillStyle = fondo;
+    context.lineWidth = 5; // grosor de linea del canvas
 
     context.fillRect(0, 0, canvas.height, canvas.width);
     context.strokeRect(0, 0, canvas.height, canvas.width);
