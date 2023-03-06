2d36107d62e1fe857c36ee51ae4f7424560a1b57
FHP187 <gjimenez@jaumebalmes.net>
2023-03-06 20:08:01 +0100

    Effects Parallax

--- UF3/Practica_parallax/js/parallax-scroll.js
+++ UF3/Practica_parallax/js/parallax-scroll.js
@@ -1,14 +1,16 @@
 (function() {
 
-const person = document.querySelector('.parallax__person');
-const container = document.querySelector('.parallax');
+    const person = document.querySelector('.parallax__person');
+    const container = document.querySelector('.parallax');
 
-window.addEventListener('scroll', parallax);
+    window.addEventListener('scroll', parallax);
 
-function parallax(){
-    const value = window.scrollY;
-    container.style.backgroundPositionY = `${value/1.1}px`;
-    person.style.backgroundPositionY = `${value/2}px`;
-}
+    function parallax(){
+        const value = window.scrollY;
+        container.style.backgroundPositionY = `${value/1.1}px`;
+        person.style.backgroundPositionY = `${value/2}px`;
+    }
 
 })();
+
+
--- UF3/Practica_parallax/js/scroll.js
+++ UF3/Practica_parallax/js/scroll.js
@@ -1,34 +1,12 @@
-$('div.parallaxBackground').each(function(){
-    var item = $(this);
-
-    item.find('article').css({
-        position:'absolute',
-        left: (item .width() - $('article').width())/2,
-        top: (item .height() - $('article').height())/2
-    });
-
-    $(window).scroll(function() {
-
-        var yPosition = -($(window).scrollTop() / item.data('scroll-speed'));
-
-        var backgroundPosition = '50% '+ yPosition + 'px';
-
-
-        item.css('background-position', backgroundPosition );
-
-
-    });
-
-    $(window).resize(function() {
-
-        item.find('article').css({
-            position:'absolute',
-            left: (item .width() - $('article').width())/2,
-            top: (item .height() - $('article').height())/2
-        });
-
-    });
-    
-
-});
-
+(function() {
+    window.addEventListener(
+    "scroll",
+    () => {
+        document.body.style.setProperty(
+            "--scroll",
+            window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+        );
+    },
+    false
+);
+})();
