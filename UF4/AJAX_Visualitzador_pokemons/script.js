8f96674796f1f54f5a87e20bc4b5909eb35158dd
FHP187 <gjimenez@jaumebalmes.net>
2023-03-29 20:31:55 +0200

    Modificaciones api pokemon

--- UF4/AJAX_Visualitzador_pokemons/script.js
+++ UF4/AJAX_Visualitzador_pokemons/script.js
@@ -6,13 +6,25 @@ function buscarPokemon(nombre) {
             var data = JSON.parse(this.responseText);
             var nombre = data.name;
             var imagen = data.sprites.front_default;
+            var imagen_art = data.sprites.other.dream_world.front_default;
+            var imagen_back = data.sprites.back_default;
+            var imagen_shinny = data.sprites.front_shiny;
+            var imagen_shinny_back = data.sprites.back_shiny;
             var tipos = data.types.map(function(tipo) {
                 return tipo.type.name;
             }).join(', ');
             var habilidades = data.abilities.map(function(habilidad) {
                 return habilidad.ability.name;
             }).join(', ');
-            var html = '<tr><td>' + nombre + '</td><td><img src="' + imagen + '"></td><td>' + tipos + '</td><td>' + habilidades + '</td></tr>';
+
+            var html = '<tr><td>' + nombre + '</td><td>' +
+                '<img width="200" height="200" src="' + imagen_art + '"></td><td>' +
+                '<img src="' + imagen + '"></td><td>' +
+                '<img src="' + imagen_back + '"></td><td>' +
+                '<img src="' + imagen_shinny + '"></td><td>' +
+                '<img src="' + imagen_shinny_back + '"></td>' +
+                '</td><td>' + tipos + '</td>' +
+                '<td>' + habilidades + '</td></tr>';
             document.getElementById('resultado').innerHTML = html;
         }
     };
